2f4b722d7f647a8b4c204f15acf75cf92d75dc02
Csr <138554277+Csr429@users.noreply.github.com>
2025-12-11 21:21:03 -0300

    adicionando os executaveis

---
+++ fn006.c
@@ -0,0 +1,16 @@
+/*
+Exercício 3: Soma dos Elementos de um Array
+Objetivo: Aplicar a recursão para processar uma estrutura de dados (array).
+
+Descrição: Implemente uma função recursiva que calcule a soma de todos os elementos em um array de inteiros.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculo(int )
+
+int main()
+{
+    system ("cls");
+}
