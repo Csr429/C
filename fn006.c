01d658a945b2434c4e18c13902f223e26566f342
Csr <138554277+Csr429@users.noreply.github.com>
2025-12-15 21:18:15 -0300

    exercicio de recursao

--- fn006.c
+++ fn006.c
@@ -8,9 +8,30 @@ Descrição: Implemente uma função recursiva que calcule a soma de todos os el
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculo(int )
+int recursao(int arr[], int tamanho);
 
 int main()
 {
-    system ("cls");
+    system("cls");
+    int i[5] = {1, 2, 3, 4, 5};
+    int soma = 0;
+
+    soma = recursao(i,5);
+
+    printf("a soma total do vetor é: %d", soma);
+
+    return 0;
+}
+
+int recursao(int arr[], int tamanho)
+{
+    if (tamanho <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return arr[tamanho - 1] + recursao(arr, tamanho-1);
+
+    }
 }
