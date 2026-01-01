9e0c79b7e3bf6499acee994288faa3b65459ca8b
Csr <138554277+Csr429@users.noreply.github.com>
2025-12-31 23:26:36 -0300

    exercício de ponteiros

--- ptr008.c
+++ ptr008.c
@@ -5,24 +5,26 @@ valor utilizando apenas os ponteiros B, C e D. O ponteiro B deve ser usada para 
 o triplo e D o quádruplo.
 */
 
-
 #include <stdio.h>
 #include <stdlib.h>
-
-void function(short int *ptr);
 
 int main()
 {
     system("cls");
 
-    short int numero = 0; 
+    short int a = 0, *b, **c, ***d;
+
+    b = &a;
+    c = &b;
+    d = &c;
+
+    printf("\n Por favor, insira um numero: ");
+    scanf("%hd", &a);
+
+    printf("\n O dobro de %hd e: %hd", a, 2 * *b);
+    printf("\n O triplo de %hd e: %hd", a, 3 * **c);
+    printf("\n O quadruplo de %hd e: %hd", a, 4 * ***d);
 
 
     return 0;
-}
-
-void function(short int *ptr)
-{
-    short int **c, ***d;
-
 }
