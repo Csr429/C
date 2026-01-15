6686b91342031a44a974aec997ccb59235c4cdca
Csr <138554277+Csr429@users.noreply.github.com>
2026-01-15 20:46:28 -0300

    Exercício para por uma string toda em um arquivo

--- file04.c
+++ file04.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void escreve(char nome);
+void escreve(char nome[]);
 
 int main()
 {
 
     system("cls");
+    escreve("..\\files\\file04.txt");
 
     return 0;
 }
 
-void escreve(char nome)
+void escreve(char nome[])
 {
     FILE *file;
     char frase[500];
 
     file = fopen(nome, "w");
 
-    if(file)
+    if (file)
     {
-        printf("\n Por favor, insira uma frase e pressione enter");
+        printf("\n Por favor, insira uma frase e pressione enter ou tecle '.' e pressione enter para finalizar. \n");
         scanf("%500[^\n]", frase);
-        
+        fflush(stdin);
+        while (strlen(frase) > 1)
+        {
+            fputs(frase, file);
+            fputc('\n', file);
+            fflush(stdin);
+            scanf("%500[^\n]", frase);
+        }
+
+        fclose(file);
     }
-    else 
+    else
     {
         printf("\n Erro ao acessar o arquivo");
     }
