ca3bd3225427604c215c7d3075f86bd39793427e
Csr <138554277+Csr429@users.noreply.github.com>
2026-02-10 20:02:19 -0300

    exercício de agenda

--- agenda.c
+++ agenda.c
@@ -20,8 +20,14 @@ int cadastrar(contato **c, int qtty, int size);
 int main()
 {
     contato *agenda[50];
-    int tam = 0, qtd = 0;
+    int tam = 50, qtd = 0;
     system("cls");
+
+    imprimir(agenda, qtd);
+    qtd += cadastrar(agenda, qtd, tam);
+    printf("\n qtd = %d \n ", qtd);
+
+    imprimir(agenda, qtd);
 
     return 0;
 }
@@ -33,20 +39,39 @@ int imprimir(contato **c, int qtty)
     printf("=====================================================================================");
     for (int i = 0; i < qtty; i++)
     {
-        printf("\n %d: %s %2d/%2d/%4d",
-               (i+1),
-               (*(c+i))->nome,
-               (*(c+i))->nascimento.dia,
-               (*(c+i))->nascimento.mes,
-               (*(c+i))->nascimento.ano);
+        printf("\n teste");
+        printf("\n %d: %s %2d/%2d/%4d %c",
+               (i + 1),
+               (*(c + i))->nome,
+               (*(c + i))->nascimento.dia,
+               (*(c + i))->nascimento.mes,
+               (*(c + i))->nascimento.ano,
+               (*(c + i))->genero);
     }
     return 1;
 }
 
-/*
 int cadastrar(contato **c, int qtty, int size)
 {
-    pritnf("\n Por favor, insira o nome completo e a data de nascimento no formato DD MM AAAA: ");
-    scanf("%[100] %d %d %d", *(c+qtty)->nome, *(c+qtty)-> nascimento.dia, *(c+qtty)-> nascimento.mes, *(c+qtty)->nascimento.ano);
+    if (qtty < size)
+    {
+        contato *novo = calloc(sizeof(contato), 1);
+        printf("\n Por favor, insira um novo contato:");
+        printf("\n Nome: ");
+        scanf("%100[^\n]", novo->nome);
+        fflush(stdin);
+        printf("\n Por favor, insira a data de nascimento no seguinte formato: dd mm aaaa\n ");
+        scanf("%d%d%d", &novo->nascimento.dia, &novo->nascimento.mes, &novo->nascimento.ano);
+        fflush(stdin);
+        printf("\n Genero(M/F): ");
+        scanf("%c", &novo->genero);
+        c[qtty] = novo;
+
+        return 1;
+    }
+    else
+    {
+        printf("\n Excedido o tamanho do vetor");
+        return 0;
+    }
 }
-    */
